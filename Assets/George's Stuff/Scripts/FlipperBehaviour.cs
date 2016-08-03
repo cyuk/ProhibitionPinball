b336925913c17ca1a3e13a4815e0217fd6209eb2
Georgeooo <george_aylott@hotmail.com>
2016-08-03 23:42:58 +0100

    Ver 0.1
    Added hinge joints with scripting

---
+++ Assets/George's Stuff/Scripts/FlipperBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipperBehaviour : MonoBehaviour {
+
+	HingeJoint flipperLHinge;
+	JointMotor motor;
+	public KeyCode button;
+	// Use this for initialization
+	void Start () 
+	{
+		GetComponent<Rigidbody> ().maxAngularVelocity = 100f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (button) || Input.GetKeyUp (button)) 
+		{
+			HingeJoint hinge = GetComponent<HingeJoint> ();
+			JointMotor motor = hinge.motor;
+			motor.targetVelocity = -motor.targetVelocity;
+			hinge.motor = motor;
+		}
+	}
+}
