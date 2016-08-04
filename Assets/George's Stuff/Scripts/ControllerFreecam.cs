568e178a9246385ea8e67024185b861d82f78bf0
Georgeooo <george_aylott@hotmail.com>
2016-08-04 23:09:39 +0100

    Ver 0.3 Added spring thingy

--- Assets/George's Stuff/Scripts/ControllerFreecam.cs
+++ Assets/George's Stuff/Scripts/ControllerFreecam.cs
@@ -14,8 +14,58 @@ public class ControllerFreecam : MonoBehaviour {
 
 	void Update () 
 	{
-		if (Input.GetKeyDown (KeyCode.LeftShift) || Input.GetKeyUp (KeyCode.LeftShift)) {
-
+		if (Input.GetKeyDown (KeyCode.LeftShift) || Input.GetKeyDown (KeyCode.RightShift)) 
+        {
+            shift = true;
+            flySpeed *= accelerationRatio;
 		}
+
+        if(Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+        {
+            shift = false;
+            flySpeed /= accelerationRatio;
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftControl)||Input.GetKeyDown(KeyCode.RightControl))
+        {
+            ctrl = true;
+            flySpeed *= slowDownRatio;
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftControl)||Input.GetKeyUp(KeyCode.RightControl))
+        {
+            ctrl = false;
+            flySpeed /= slowDownRatio;
+        }
+
+        if(Input.GetAxis("Vertical") != 0)
+        {
+            transform.Translate(-defaultCamera.transform.forward * flySpeed * Input.GetAxis("Vertical"));
+        }
+
+        if(Input.GetAxis("Horizontal") != 0)
+        {
+            transform.Translate(defaultCamera.transform.right * flySpeed * Input.GetAxis("Horizontal"));
+        }
+
+        if(Input.GetKey(KeyCode.E))
+        {
+            transform.Translate(defaultCamera.transform.up * flySpeed * 0.5f);
+        }
+
+        if(Input.GetKey(KeyCode.Q))
+        {
+            transform.Translate(-defaultCamera.transform.up * flySpeed * 0.5f);
+        }
+
+        //if(Input.GetKeyDown(KeyCode.F12))
+       // {
+       //     switchCamera();
+       // }
+
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            playerObject.transform.position = transform.position;
+        }
 	}
 }
