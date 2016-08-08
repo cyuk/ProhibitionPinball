99133b5f73b93373157a07913cd70a6b4603a5b9
Georgeooo <george_aylott@hotmail.com>
2016-08-08 20:23:15 +0100

    Ball firer working properly

--- Assets/George's Stuff/Scripts/PullBack.cs
+++ Assets/George's Stuff/Scripts/PullBack.cs
@@ -6,7 +6,7 @@ public class PullBack : MonoBehaviour {
     public Rigidbody pusherRigid;
     public float push;
     public Vector3 offset;
-
+    public Vector3 dicks;
 	void Start () 
     {
         //Debug.Log("Messagw");
@@ -15,12 +15,26 @@ public class PullBack : MonoBehaviour {
 	
 	void Update () 
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Debug.Log("PullBack Spring disabled");
+            GetComponent<SpringJoint>().spring = 0.0f;
+        }
        // Debug.Log("IAHSOONHAOfso");
 	    if(Input.GetKey(KeyCode.Space))
         {
             Debug.Log("PushBack if.getKey case true");
             //pusherRigid.AddForce(transform.forward * push);
             pusherRigid.AddForceAtPosition((-Vector3.forward).normalized * push, transform.position + offset);
+
+            //transform.Translate(offset * Time.deltaTime);
+            
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            //Debug.Log("PullBack Spring enabled");
+            GetComponent<SpringJoint>().spring = 1500.0f;
         }
 	}
 }
